e3fd77afda8ccbf263645094424b59b5a87500c3
Duyên <duyen0602tm@gmail.com>
2025-05-22 01:17:53 +0700

    feat: implement NotFoundComponent for 404 error page and simplify NotFound page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next';
-import Link from 'next/link';
+
+import NotFoundComponent from '@/components/404/NotFoundComponent';
 
 export const metadata: Metadata = {
 	title: 'Rose Shop | 404 - Not Found',
@@ -8,22 +9,7 @@ export const metadata: Metadata = {
 };
 
 const NotFound = () => {
-	return (
-		<>
-			<div className="flex flex-col items-center justify-center h-screen">
-				<h1 className="text-4xl font-bold text-[#FF6B81]">404 - Not Found</h1>
-				<p className="mt-4 text-lg text-[#FF6B81]">
-					The page you are looking for does not exist.
-				</p>
-				<Link
-					href="/"
-					className="mt-6 px-4 py-2 bg-[#FF6B81] text-white rounded hover:bg-[#FF9EAA] hover:text-white transition-colors"
-				>
-					Return Home
-				</Link>
-			</div>
-		</>
-	);
+	return <NotFoundComponent />;
 };
 
 export default NotFound;
