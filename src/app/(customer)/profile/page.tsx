1fed4b491ef25418725065fdca6991f8eb16878c
Duyên <duyen0602tm@gmail.com>
2025-06-17 10:11:32 +0700

    feat: Implement user authentication and profile management with ProtectedRoute and userService

--- src/app/(customer)/profile/page.tsx
+++ src/app/(customer)/profile/page.tsx
@@ -1,11 +1,14 @@
+import ProtectedRoute from '@/components/common/ProtectedRoute';
 import UserProfileView from '@/components/customer/profile/UserProfileView';
 
 const UserProfile = () => {
-  return (
-    <div className="mx-auto pt-24">
-      <UserProfileView />
-    </div>
-  );
+	return (
+		<ProtectedRoute>
+			<div className="mx-auto pt-24">
+				<UserProfileView />
+			</div>
+		</ProtectedRoute>
+	);
 };
 
 export default UserProfile;
